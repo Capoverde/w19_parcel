0c4d42bbb08104eb329f8a850e4963d247a52f31
Piotr <piotr_as@yahoo.com>
2021-11-25 15:37:02 +0100

    wypozycjonowany soundbar, dopracować slidera i sound visualizer

--- src/js/index.js
+++ src/js/index.js
@@ -64,80 +64,110 @@ const speakers = document.querySelectorAll('#speaker')
 /*
 *                  SPECTROGRAPH
 =============================================== */
-(function () {
+// (function () {
 
-  // The number of bars that should be displayed
-  const NBR_OF_BARS = 50;
+//   // The number of bars that should be displayed
+//   const NBR_OF_BARS = 50;
 
-  // Get the audio element tag
-  const audio = document.querySelector("audio");
+//   // Get the audio element tag
+//   const audio = document.querySelector("audio");
 
-  // Create an audio context
-  const ctx = new AudioContext();
+//   // Create an audio context
+//   const ctx = new AudioContext();
 
-  // Create an audio source
-  const audioSource = ctx.createMediaElementSource(audio);
+//   // Create an audio source
+//   const audioSource = ctx.createMediaElementSource(audio);
 
-  // Create an audio analyzer
-  const analayzer = ctx.createAnalyser();
+//   // Create an audio analyzer
+//   const analayzer = ctx.createAnalyser();
 
-  // Connect the source, to the analyzer, and then back the the context's destination
-  audioSource.connect(analayzer);
-  audioSource.connect(ctx.destination);
+//   // Connect the source, to the analyzer, and then back the the context's destination
+//   audioSource.connect(analayzer);
+//   audioSource.connect(ctx.destination);
 
-  // Print the analyze frequencies
-  const frequencyData = new Uint8Array(analayzer.frequencyBinCount);
-  analayzer.getByteFrequencyData(frequencyData);
-  console.log("frequencyData", frequencyData);
+//   // Print the analyze frequencies
+//   const frequencyData = new Uint8Array(analayzer.frequencyBinCount);
+//   analayzer.getByteFrequencyData(frequencyData);
+//   console.log("frequencyData", frequencyData);
 
-  // Get the visualizer container
-  const visualizerContainer = document.querySelector(".visualizer-container");
+//   // Get the visualizer container
+//   const visualizerContainer = document.querySelector(".visualizer-container");
 
-  // Create a set of pre-defined bars
-  for( let i = 0; i < NBR_OF_BARS; i++ ) {
+//   // Create a set of pre-defined bars
+//   for( let i = 0; i < NBR_OF_BARS; i++ ) {
 
-      const bar = document.createElement("DIV");
-      bar.setAttribute("id", "bar" + i);
-      bar.setAttribute("class", "visualizer-container__bar");
-      visualizerContainer.appendChild(bar);
+//       const bar = document.createElement("DIV");
+//       bar.setAttribute("id", "bar" + i);
+//       bar.setAttribute("class", "visualizer-container__bar");
+//       visualizerContainer.appendChild(bar);
 
-  }
+//   }
 
-  // This function has the task to adjust the bar heights according to the frequency data
-  function renderFrame() {
+//   // This function has the task to adjust the bar heights according to the frequency data
+//   function renderFrame() {
 
-      // Update our frequency data array with the latest frequency data
-      analayzer.getByteFrequencyData(frequencyData);
+//       // Update our frequency data array with the latest frequency data
+//       analayzer.getByteFrequencyData(frequencyData);
 
-      for( let i = 0; i < NBR_OF_BARS; i++ ) {
+//       for( let i = 0; i < NBR_OF_BARS; i++ ) {
 
-          // Since the frequency data array is 1024 in length, we don't want to fetch
-          // the first NBR_OF_BARS of values, but try and grab frequencies over the whole spectrum
-          const index = (i + 10) * 2;
-          // fd is a frequency value between 0 and 255
-          const fd = frequencyData[index];
+//           // Since the frequency data array is 1024 in length, we don't want to fetch
+//           // the first NBR_OF_BARS of values, but try and grab frequencies over the whole spectrum
+//           const index = (i + 10) * 2;
+//           // fd is a frequency value between 0 and 255
+//           const fd = frequencyData[index];
 
-          // Fetch the bar DIV element
-          const bar = document.querySelector("#bar" + i);
-          if( !bar ) {
-              continue;
-          }
+//           // Fetch the bar DIV element
+//           const bar = document.querySelector("#bar" + i);
+//           if( !bar ) {
+//               continue;
+//           }
 
-          // If fd is undefined, default to 0, then make sure fd is at least 4
-          // This will make make a quiet frequency at least 4px high for visual effects
-          const barHeight = Math.max(4, fd || 0);
-          bar.style.height = barHeight + "px";
+//           // If fd is undefined, default to 0, then make sure fd is at least 4
+//           // This will make make a quiet frequency at least 4px high for visual effects
+//           const barHeight = Math.max(4, fd || 0);
+//           bar.style.height = barHeight + "px";
 
-      }
+//       }
 
-      // At the next animation frame, call ourselves
-      window.requestAnimationFrame(renderFrame);
+//       // At the next animation frame, call ourselves
+//       window.requestAnimationFrame(renderFrame);
 
-  }
+//   }
 
-  renderFrame();
+//   renderFrame();
 
-  audio.volume = 0.10;
-  audio.play();
+//   audio.volume = 0.10;
+//   audio.play();
 
-})();
+// })();
+
+
+/*                  SLIDER
+=============================================== */
+
+
+
+// $(function(){
+//   $('#simple-vertical').royalSlider({
+//     arrowsNav: true,
+//     arrowsNavAutoHide: false,
+//     fadeinLoadedSlide: true,
+//     controlNavigation: 'none',
+//     imageScaleMode: 'fill',
+//     imageAlignCenter:true,
+//     loop: false,
+//     loopRewind: false,
+//     numImagesToPreload: 4,
+//     slidesOrientation: 'vertical',
+//     keyboardNavEnabled: true,
+//     video: {
+//       autoHideArrows:true,
+//       autoHideControlNav:true
+//     },  
+
+//     autoScaleSlider: true, 
+//     autoScaleSliderWidth: 960,     
+//     autoScaleSliderHeight: 850
+//   });
+// })
